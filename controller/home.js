81713f75dab18f3de84c67d59dc5784683753186
Krum Kuzmov <kkuzmov@gmail.com>
2021-04-29 17:02:54 +0300

    fix search in homepage

--- controller/home.js
+++ controller/home.js
@@ -5,28 +5,27 @@ import commonPartials from './partials.js';
 export function getHome(context) {
         setHeader(context)
         getAll()
-        .then(res =>{
-                let recipes = res.docs.map(recipe => recipe = {...recipe.data(), id: recipe.id});
-                recipes.length > 0 ? context.recipes = true : context.recipes = false;
+                .then(res =>{
+                        let recipes = res.docs.map(recipe => recipe = {...recipe.data(), id: recipe.id});
+                        recipes.length > 0 ? context.recipes = true : context.recipes = false;
 
-                context.vegetables = recipes.filter(rec => rec.category.startsWith('Vegetables'));
-                context.meatsFish = recipes.filter(rec => rec.category.startsWith('Lean'));
-                context.fruits = recipes.filter(rec => rec.category.startsWith('Fruits'))
-                context.grain = recipes.filter(rec => rec.category.startsWith('Grain'))
-                context.milk = recipes.filter(rec => rec.category.startsWith('Milk'))        
+                        context.vegetables = recipes.filter(rec => rec.category.startsWith('Vegetables'));
+                        context.meatsFish = recipes.filter(rec => rec.category.startsWith('Lean'));
+                        context.fruits = recipes.filter(rec => rec.category.startsWith('Fruits'))
+                        context.grain = recipes.filter(rec => rec.category.startsWith('Grain'))
+                        context.milk = recipes.filter(rec => rec.category.startsWith('Milk'))        
 
-                context.loadPartials(commonPartials).partial('../templates/user/home.hbs');
+                        context.loadPartials(commonPartials).partial('../templates/user/home.hbs');
         })
 }
 export function searchMeal(event, context){
+        setHeader(context)
+
         let searchQuery = event.target.firstElementChild.value;
         getAll()
-        .then(res =>{
-                res.docs.forEach(element => {
-                console.log(element.data())
-                });
-                let searchResult = res.docs.filter(x => x.data().meal.toLowerCase().includes(searchQuery.toLowerCase())).map(x => x = x.data());
-                console.log(searchResult)
-                context.result = searchResult;
-        })
+                .then(res =>{
+                        let searchResult = res.docs.filter(x => x.data().meal.toLowerCase().includes(searchQuery.toLowerCase())).map(x => x = x.data());
+                        context.result = searchResult;
+                        context.loadPartials(commonPartials).partial('../templates/functional/searchRes.hbs');
+                })
 }
